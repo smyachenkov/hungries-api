b521c895020a7c9179de10693be206b917ca26d1
smyachenkov <s.myachenkov@gmail.com>
2021-09-07 23:07:53 +0300

    fix migration 7

--- db/migrations/7_likes_rename_userid.up.sql
+++ db/migrations/7_likes_rename_userid.up.sql
@@ -1,3 +1,2 @@
-alter table hungries.like
-    alter column device_id type text
-    rename device_id to user_id;
+alter table hungries.like alter column device_id type text;
+alter table hungries.like rename device_id to user_id;
